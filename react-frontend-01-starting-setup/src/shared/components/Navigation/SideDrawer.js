65a14d88ed9654b3a652b29fef2379a25f8c4698
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-01-20 18:01:44 -0600

    Create react portal to render the side element beside the root html

---
+++ react-frontend-01-starting-setup/src/shared/components/Navigation/SideDrawer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './SideDrawer.css';
+import ReactDom from "react-dom";
+
+const SideDrawer = props => {
+  const content =  <aside className="side-drawer">{props.children}</aside>
+  return ReactDom.createPortal(content, document.getElementById("drawer-hook"));
+};
+
+export default SideDrawer;
