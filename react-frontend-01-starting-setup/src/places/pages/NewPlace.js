2d091ce353f5dcdfbb2b80dacea4340fd0ab4b40
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-01-21 23:57:05 -0600

    Create custom form hook

--- react-frontend-01-starting-setup/src/places/pages/NewPlace.js
+++ react-frontend-01-starting-setup/src/places/pages/NewPlace.js
@@ -7,34 +7,11 @@ import {
     VALIDATOR_MINLENGTH
 } from '../../shared/util/validators';
 import './PlaceForm.css';
-
-const formReducer = (state, action) => {
-    switch (action.type) {
-        case 'INPUT_CHANGE':
-            let formIsValid = true;
-            for (const inputId in state.inputs) {
-                if (inputId === action.inputId) {
-                    formIsValid = formIsValid && action.isValid;
-                } else {
-                    formIsValid = formIsValid && state.inputs[inputId].isValid;
-                }
-            }
-            return {
-                ...state,
-                inputs: {
-                    ...state.inputs,
-                    [action.inputId]: {value: action.value, isValid: action.isValid}
-                },
-                isValid: formIsValid
-            };
-        default:
-            return state;
-    }
-};
+import {useForm} from "../../shared/hooks/form-hook";
 
 const NewPlace = () => {
-    const [formState, dispatch] = useReducer(formReducer, {
-        inputs: {
+    const [formState, inputHandler] = useForm(
+        {
             title: {
                 value: '',
                 isValid: false
@@ -47,18 +24,10 @@ const NewPlace = () => {
                 value: '',
                 isValid: false
             }
-        },
-        isValid: false
-    });
+        }, false
+    );
 
-    const inputHandler = useCallback((id, value, isValid) => {
-        dispatch({
-            type: 'INPUT_CHANGE',
-            value: value,
-            isValid: isValid,
-            inputId: id
-        });
-    }, []);
+
 
     const placeSubmitHandler = event => {
         event.preventDefault();
--- react-frontend-01-starting-setup/src/places/pages/UpdatePlace.js
+++ react-frontend-01-starting-setup/src/places/pages/UpdatePlace.js
@@ -4,44 +4,48 @@ import {useParams} from 'react-router-dom';
 import Input from "../../shared/components/FormElements/Input";
 import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from "../../shared/util/validators";
 import Button from "../../shared/components/FormElements/Button";
+import {useForm} from "../../shared/hooks/form-hook";
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-        address: '20 W 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u1'
+const DUMMY_PLACES = [{
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+        lat: 40.7484405, lng: -73.9878584
     },
-    {
-        id: 'p2',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-        address: '20 W 34th St, New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u2'
-    }
-];
+    creator: 'u1'
+}, {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+        lat: 40.7484405, lng: -73.9878584
+    },
+    creator: 'u2'
+}];
 
 
 const UpdatePlace = () => {
     const placeId = useParams().placeId;
     const identifiedPlace = DUMMY_PLACES.find((place) => place.id === placeId);
 
-    if(!identifiedPlace){
+    const [formState, inputHandler] = useForm({
+        title: {
+            value: identifiedPlace.title, isValid: true
+        }, description: {
+            value: identifiedPlace.description, isValid: true
+        }
+    }, true);
+    if (!identifiedPlace) {
         return <div className="center">
             <h2>Could not find place!</h2>
         </div>
     }
+    console.log('update place', formState, identifiedPlace);
     return (<form className='place-form'>
         <Input
             id="title"
@@ -50,8 +54,10 @@ const UpdatePlace = () => {
             label="Title"
             validators={[VALIDATOR_REQUIRE()]}
             errorText="Please enter a valid title."
-            onInput={() => {}}
-            value={identifiedPlace.title}
+            onInput={inputHandler}
+            initialValue={formState.inputs.title.value}
+            initialIsValid={formState.inputs.title.isValid}
+
         />
         <Input
             id="description"
@@ -59,11 +65,12 @@ const UpdatePlace = () => {
             label="Description"
             validators={[VALIDATOR_MINLENGTH(5)]}
             errorText="Please enter a valid description (at least 5 characters)."
-            onInput={() => {}}
-            value={identifiedPlace.description}
+            onInput={inputHandler}
+            initialValue={formState.inputs.description.value}
+            initialIsValid={formState.inputs.description.isValid}
         />
 
-        <Button type="submit">
+        <Button type="submit" disable={!formState.isValid}>Submit
             UPDATE PLACES
         </Button>
     </form>)
