4d0fa5368724b98c464f11f6a1fc0b2a52296769
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-01-21 22:18:57 -0600

    Fix bugs

--- react-frontend-01-starting-setup/src/places/pages/NewPlace.js
+++ react-frontend-01-starting-setup/src/places/pages/NewPlace.js
@@ -1,79 +1,85 @@
 import React, {useCallback, useReducer} from 'react';
-import Input from "../../shared/components/FormElements/Input";
+
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import {
+    VALIDATOR_REQUIRE,
+    VALIDATOR_MINLENGTH
+} from '../../shared/util/validators';
 import './NewPlace.css';
-import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from "../../shared/util/validators";
-import Button from "../../shared/components/FormElements/Button";
 
 const formReducer = (state, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case 'INPUT_CHANGE':
             let formIsValid = true;
-            for(const inputId in state.inputs) {
-                if(inputId === action.inputId){
+            for (const inputId in state.inputs) {
+                if (inputId === action.inputId) {
                     formIsValid = formIsValid && action.isValid;
-                }else {
+                } else {
                     formIsValid = formIsValid && state.inputs[inputId].isValid;
                 }
             }
             return {
                 ...state,
-                input: {
-                    ...state.inputs, [action.inputId] : {value: action.value, isValid: action.isValid},
+                inputs: {
+                    ...state.inputs,
+                    [action.inputId]: {value: action.value, isValid: action.isValid}
                 },
-                isValid: formIsValid,
-            }
+                isValid: formIsValid
+            };
         default:
             return state;
     }
+};
 
-}
-
-const NewPlace = (props) => {
-
+const NewPlace = () => {
     const [formState, dispatch] = useReducer(formReducer, {
-        input: {
-            value: "",
-            isValid: false,
+        inputs: {
+            title: {
+                value: '',
+                isValid: false
+            },
+            description: {
+                value: '',
+                isValid: false
+            }
         },
-        description: {
-            value: "",
-            isValid: false,
-        },
-        isValid: false,
-    },)
+        isValid: false
+    });
 
     const inputHandler = useCallback((id, value, isValid) => {
         dispatch({
             type: 'INPUT_CHANGE',
-            value:value,
-            isValid,
+            value: value,
+            isValid: isValid,
             inputId: id
         });
-    },[dispatch]);
+    }, []);
 
-  return (
-      <form className="place-form" onSubmit={props.onSubmit}>
-       <Input
-           id="title"
-           element="input"
-           type="text"
-           label="Title"
-           validators={[VALIDATOR_REQUIRE()]}
-           errorText="Please enter a title"
-           onInput={inputHandler}
-       />
-          <Input
-              id="description"
-              element="textarea"
-              label="Description"
-              validators={[VALIDATOR_MINLENGTH(5)]}
-              errorText="Please enter a description (at least 5 characters)"
-              onInput={inputHandler}
-          />
-          <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
-
-      </form>
-  )
+    return (
+        <form className="place-form">
+            <Input
+                id="title"
+                element="input"
+                type="text"
+                label="Title"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter a valid title."
+                onInput={inputHandler}
+            />
+            <Input
+                id="description"
+                element="textarea"
+                label="Description"
+                validators={[VALIDATOR_MINLENGTH(5)]}
+                errorText="Please enter a valid description (at least 5 characters)."
+                onInput={inputHandler}
+            />
+            <Button type="submit" disabled={!formState.isValid}>
+                ADD PLACE
+            </Button>
+        </form>
+    );
 };
 
 export default NewPlace;
