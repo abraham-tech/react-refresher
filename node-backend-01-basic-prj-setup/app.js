6c79f20a5fa92d428cd5522c326e5cb704d49289
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-01-23 00:55:33 -0600

    Get place by id

--- node-backend-01-basic-prj-setup/app.js
+++ node-backend-01-basic-prj-setup/app.js
@@ -7,6 +7,6 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(bodyParser.json());
-app.use(placesRouter);
+app.use('/api/places', placesRouter); // api/places
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
