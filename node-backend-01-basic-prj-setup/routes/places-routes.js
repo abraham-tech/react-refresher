ed96a75ec6d43f8ae28c478dcc1e4ceb18efc56a
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-01-24 17:39:56 -0600

    Add patch and Update routes to places endpoint

--- node-backend-01-basic-prj-setup/routes/places-routes.js
+++ node-backend-01-basic-prj-setup/routes/places-routes.js
@@ -1,25 +1,17 @@
 const express = require('express');
-const {getPlaceById, getPlaceByUserId} = require("../controllers/places-controllers");
 
+const placesControllers = require('../controllers/places-controllers');
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516
-    },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
-  }
-];
+router.get('/:pid', placesControllers.getPlaceById);
 
-router.get('/:pid', getPlaceById);
+router.get('/user/:uid', placesControllers.getPlaceByUserId);
 
-router.get('/user/:uid', getPlaceByUserId)
+router.post('/', placesControllers.createPlace);
+
+router.patch('/:pid', placesControllers.updatePlace);
+
+router.delete('/:pid', placesControllers.deletePlace);
 
 module.exports = router;
