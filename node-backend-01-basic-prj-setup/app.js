1f254384b996630c089f8b62eb4f0704fcd15115
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-01-26 15:01:21 -0600

    Connecting front end with backend

--- node-backend-01-basic-prj-setup/app.js
+++ node-backend-01-basic-prj-setup/app.js
@@ -10,7 +10,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/places', placesRoutes); // => /api/places...
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
+app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
@@ -22,12 +33,19 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred!'});
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017').then((db) => {
-  app.listen(5001);
-}).catch((err) => {
-  console.log(err);
-})
+const uri = 'mongodb://admin:password@localhost:27017/mydb?authSource=admin';
+
+mongoose
+    .connect(
+        process.env.MONGODB_URI || uri,
+    )
+    .then(() => {
+      app.listen(5001);
+    })
+    .catch(err => {
+      console.log(err);
+    });
--- node-backend-01-basic-prj-setup/controllers/users-controllers.js
+++ node-backend-01-basic-prj-setup/controllers/users-controllers.js
@@ -60,7 +60,7 @@ const signup = async (req, res, next) => {
     email,
     image: 'https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg',
     password,
-    places
+    // places
   });
 
   try {
